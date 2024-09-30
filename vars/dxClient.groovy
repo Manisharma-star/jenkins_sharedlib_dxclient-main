79c7090a115e8d458542a09f489282c522effcfa
Manisharma-star <manisharma1991@gmail.com>
2024-09-30 17:31:36 +0530

    Update dxClient.groovy

--- vars/dxClient.groovy
+++ vars/dxClient.groovy
@@ -53,7 +53,7 @@ def call() {
                   Exception caughtException = null;
               catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
                 try {
-                  command = "deploy-application -hostname ${config.hostname} -dxProtocol ${config.dxProtocol} -dxPort ${config.dxPort} -dxUsername wpsadmin -dxPassword Avn3tNPR -dxConnectPort ${config.dxPort} -dxConnectUsername wpsadmin -dxConnectPassword Avn3tNPR -applicationFile Deployables\\EAR\\fspappinterfaceEAR.ear -applicationName fspappinterfaceEAR -dxProfileName ${dxProfileName}"
+                  command = "deploy-application -hostname ${config.hostname} -dxProtocol ${config.dxProtocol} -dxPort ${config.dxPort} -dxUsername wpsadmin -dxPassword Avn3tNPR -dxConnectPort ${config.dxPort} -dxConnectUsername wpsadmin -dxConnectPassword Avn3tNPR -applicationFile Deployables\\EAR\\fspappinterfaceEAR.ear -applicationName fspappinterfaceEAR -dxProfileName ${config.dxProfileName}"
 
               // TODO : check for generic artifact path   
                   sh "./bin/dxclient ${command}"
